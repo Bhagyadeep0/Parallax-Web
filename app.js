75d0a0502139135ff0eb9321aa3e1234eaa4fa7c
Bhagyadeep <b.ghosh0330@gmail.com>
2024-02-15 11:53:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let moon = document.getElementById("moon");
+let stars = document.getElementById("stars");
+let bigLand = document.getElementById("bg-land");
+let midLand = document.getElementById("mid-land");
+let landGlow = document.getElementById("landscape-glow");
+let foreLand = document.getElementById("fore-land");
+let moonGlow = document.getElementById("moon-glow");
+let text = document.getElementById("text");
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  stars.style.left = scroll * 0.1 + "px";
+  moon.style.top = scroll * 1.2 + "px";
+  moonGlow.style.top = scroll * 1.2 + "px";
+  moonGlow.style.zIndex = "-8";
+  bigLand.style.top = scroll * 0.5 + "px";
+  midLand.style.top = scroll * 0.35 + "px";
+  landGlow.style.top = scroll * 0.2 + "px";
+  foreLand.style.top = scroll * 0.2 + "px";
+  text.style.marginRight = scroll * 4 + "px";
+  text.style.marginTop = scroll * 1.2 + "px";
+});
